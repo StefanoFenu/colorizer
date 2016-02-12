0b6d0a2e4670db29aac5a8c49ecee542de449e62
Ubuntu <ubuntu@ip-172-31-23-209.us-west-2.compute.internal>
2016-02-12 04:13:51 +0000

    Added requirements.txt, modified install.sh to include pointer to dependency list

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 virtualenv .
 source bin/activate
-pip install -r requirements
+pip install -r requirements.txt
 pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
